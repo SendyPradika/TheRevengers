c7955f136e53f31b0bae43ab37af6acd99d35fc5
Sendy <sendypradikaputra@gmail.com>
2023-06-22 19:44:08 +0700

    update home

---
+++ app/src/main/java/com/revengers/myapplication/DetailProdukActivity.kt
@@ -0,0 +1,11 @@
+package com.revengers.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailProdukActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_produk)
+    }
+}
